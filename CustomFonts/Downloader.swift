f1c736d93e0767748680844b732c5114cb27340b
Omar Sinan <omaro.hashim@hotmail.com>
2018-03-27 21:26:52 +0300

    fixed for swift 4

--- CustomFonts/Downloader.swift
+++ CustomFonts/Downloader.swift
@@ -2,7 +2,8 @@
 //  Downloader.swift
 //  CustomFonts
 //
-//  Created by Trum Gyorgy on 12/1/15.
+//  Originally created by Trum Gyorgy on 12/1/15.
+//  Modified for Swift 4 by Omar Sinan on 27/3/18.
 //  Copyright (c) 2015 General Electric. All rights reserved.
 //
 
@@ -12,44 +13,41 @@ import CoreText
 import UIKit
 
 class Downloader {
-    class func load(URL: NSURL) -> UIFont? {
-        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
-        let session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-        let request = NSMutableURLRequest(URL: URL)
-        request.HTTPMethod = "GET"
-        let task = session.dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+    class func load(URL: NSURL , completion: @escaping () -> ()) {
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        let request = NSMutableURLRequest(url: URL as URL)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {data,response,error in
             if (error == nil) {
                 // Success
-                let statusCode = (response as! NSHTTPURLResponse).statusCode
-                println("Success: \(statusCode)")
+                let statusCode = (response as! HTTPURLResponse).statusCode
+                print("Success: \(statusCode)")
                 
                 // This is your file-variable:
                 // data
                 
                 var uiFont : UIFont?
                 let fontData = data
-
-                let dataProvider = CGDataProviderCreateWithCFData(fontData)
-                let cgFont = CGFontCreateWithDataProvider(dataProvider)
+                
+                let dataProvider = CGDataProvider(data: fontData as! CFData)
+                let cgFont = CGFont(dataProvider!)
                 
                 var error: Unmanaged<CFError>?
-                if !CTFontManagerRegisterGraphicsFont(cgFont, &error)
+                if !CTFontManagerRegisterGraphicsFont(cgFont!, &error)
                 {
                     print("Error loading Font!")
                 } else {
-                    let fontName = CGFontCopyPostScriptName(cgFont)
-                    uiFont = UIFont(name: String(fontName) , size: 30)
-
+                    let fontName = cgFont?.postScriptName
+                    uiFont = UIFont(name: String(describing: fontName) , size: 30)
+                    completion()
                 }
                 
-            }
-            else {
+            } else {
                 // Failure
-                println("Faulure: %@", error.localizedDescription);
+                print("Faulure: %@", error?.localizedDescription);
             }
-        })
+        }
         task.resume()
-        
-        return nil
     }
 }
